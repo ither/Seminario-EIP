3b0d39bcf83dfa948422497402959126f28f290d
Pablo Rochás <pablo.e.rochas@gmail.com>
2014-06-17 12:25:35 -0300

    last

--- mailchecker-snipets/snipet1.java
+++ mailchecker-snipets/snipet1.java
@@ -1,21 +1,20 @@
 public class MailSenderController
 {
-    ...
+    // ...
     private final MessagingTemplate template = new MessagingTemplate();
 
     @Autowired
     MessageChannel sendChannel;
-    ...
+    // ...
     
     private long processRequest(MailRequest req)
     {
-      // Arma el mail
-      final MimeMessage mimeMessage = mailSender.createMimeMessage();
-      ...
-      Message<MimeMessage> mailMessage = MessageBuilder.withPayload(mimeMessage)
-              .setHeader(MailCheckerHeaders.REQUEST_ID, reqId)
-              .setHeader(MailCheckerHeaders.MATCH_TYPE, "sent")
-              .build();
-      template.send(sendChannel, mailMessage);      
+        for (String mailAccount : mailRequest.getMailAccounts())
+        {
+      		// Arma el mail
+      		final MimeMessage mimeMessage = mailSender.createMimeMessage();
+      		// ...
+				integrationMailSender.sendMail(reqId, smtpId, poller, mimeMessage);
+		  }
     }
 }
