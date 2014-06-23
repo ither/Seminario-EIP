db48b1afad4c128751accc51864e73b92dc69796
Pablo Rochás <pablo.e.rochas@gmail.com>
2014-06-23 17:20:25 -0300

    Bastante completo el ejemplo de mailSender

---
+++ mailchecker-snipets/sp-int-16.java
@@ -0,0 +1,25 @@
+public class MailSenderController
+{
+    ...
+    private final MessagingTemplate template = new MessagingTemplate();
+
+    @Autowired
+    MessageChannel sendChannel;
+    ...
+
+    private long processRequest(MailRequest req)
+    {
+         // Arma el mail
+         final MimeMessage mimeMessage = mailSender.createMimeMessage();
+         ...
+         mimeMessage.setHeader("X-Mailchecker", reqId);
+         ...
+         Message<MimeMessage> mailMessage = MessageBuilder.withPayload(mimeMessage)
+         .setHeader(MailCheckerHeaders.MATCH_TYPE, "sent")
+         .setHeader(MailCheckerHeaders.REQUEST_ID, reqId)
+         .setHeader(MailCheckerHeaders.SENDER, sender)
+         .setHeader(MailCheckerHeaders.POLLER, poller)
+         .build();
+         template.send(sendChannel, mailMessage);
+    }
+}
